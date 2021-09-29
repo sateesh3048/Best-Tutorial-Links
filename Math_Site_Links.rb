1272707eb8e11be24add81f6f4f98e2d43c50e2f
sateesh3048 <sateesh.kambhampati8@gmail.com>
2021-09-29 08:16:54 +0530

    Update Math_Site_Links.rb

--- Math_Site_Links.rb
+++ Math_Site_Links.rb
@@ -4,3 +4,13 @@ https://www.mathsisfun.com/algebra/index.html
 BESt DS LINK
 
 https://www.youtube.com/playlist?list=PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O
+
+Exact Trigonometric Values using Hand Trick
+
+https://www.youtube.com/watch?v=TyrM8G1MqiI
+
+Horners method
+
+https://www.geeksforgeeks.org/horners-method-polynomial-evaluation/
+
+https://www.youtube.com/watch?v=Z393AcN_Gz0
